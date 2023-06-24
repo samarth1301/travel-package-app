467ded0568bd8c5930c46a68f1b8863b7e81784f
Shivama <shivamasthakur.02@gmail.com>
2023-06-24 19:18:44 +0530

    Home Page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+
 import Navbar from './components/Navbar.js';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path = "/" exact element={<LandingPage/>}/>
+          <Route path = "/home" exact element={<HomePage/>}/>
         </Routes>
         <Footer/>
       </Router>
