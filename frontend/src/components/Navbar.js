752e1eb8ba1591de66c2933bfa148f8fb84cfe27
Shivama <shivamasthakur.02@gmail.com>
2023-06-27 23:10:01 +0530

    responsive

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,43 +1,99 @@
+
+
 import React from "react";
-import logo from '../img/logo-100.png'
+import logo from '../img/logo-100.png';
+
 function Navbar() {
-    const user = JSON.parse(localStorage.getItem("currentUser"));
+  const user = JSON.parse(localStorage.getItem("currentUser"));
 
-    function logout() {
-        localStorage.removeItem("currentUser")
-        window.location.href="/login";
-    }
+  function logout() {
+    localStorage.removeItem("currentUser");
+    window.location.href = "/login";
+  }
 
-    return (
-        <div className="header">
-            <nav className="nav d-flex align-items-center justify-content-center gap-5">
-                <h1 className="nav-title"><a href="/home"><img className="me-2" width = "75" height = "75" src ={logo}/>TourEase</a></h1>
+  return (
+    <div className="header pb-3">
+      <nav className="navbar ps-5 navbar-expand-lg navbar-dark header">
+        <h1 className="nav-title">
+            <a href="/home" className="navbar-brand">
+            <img className="me-2" width="75" height="75" src={logo} alt="TourEase Logo" />
+            TourEase
+            </a>
+        </h1>
+        
 
-                <div className="nav-menu">
-                    <a href="/home" className="home">Home</a>
-                    <a href="/about" className="about">About</a>
-                    <a href="/contact" className="about">Contact</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                    {user ? (<>
-                        <div className="dropdown">
-                            <button className="nav-name" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className="fa-solid fa-user"></i>
-                            {user.name}
-                            <i className="fa-sharp fa-solid fa-caret-down"></i>
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a className="dropdown-item" href="/profile">Profile</a>
-                                <a className="dropdown-item" href="#" onClick={logout}>Logout</a>
-                            </div>
-                        </div>
-                    </>) : (<>
-                        <a href="/register" className="pay">Register</a>
-                        <a href="/login" className="pay">Login</a>
-                    </>)}
-
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav nav-menu ml-auto me-5 d-flex justify-content-center align-items-center">
+            <li className="nav-item">
+              <a href="/home" className="nav-link home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="nav-link about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/contact" className="nav-link contact">
+                Contact
+              </a>
+            </li>
+            {user ? (
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle text-light"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <i className="fa-solid fa-user"></i>
+                  {user.name}
+                  <i className="fa-sharp fa-solid fa-caret-down"></i>
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="/profile">
+                    Profile
+                  </a>
+                  <a className="dropdown-item" href="#" onClick={logout}>
+                    Logout
+                  </a>
                 </div>
-            </nav>
+              </li>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <a href="/register" className="nav-name">
+                    Register
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="/login" className="nav-name">
+                    Login
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
+
 export default Navbar;
--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -4,6 +4,18 @@ function AboutPage() {
   return (
     <div className="about">
       <div className="content-title mb-5">About Us</div>
+      <div className="container mb-5 custcolor">
+        TourEase is your go-to destination for seamless travel experiences. We
+        are passionate about creating unforgettable moments and lifelong
+        memories for our travelers. With a dedicated team of travel experts, we
+        strive to provide exceptional services tailored to your preferences. At
+        TourEase, we understand that every traveler is unique, seeking their own
+        personalized journey. Whether you're an adventure enthusiast, a cultural
+        explorer, or a leisure seeker, we have a wide range of carefully crafted
+        tours and itineraries to fulfill your wanderlust. Experience the
+        TourEase difference and let us redefine your travel expectations. Book
+        your next adventure with us and let the journey begin.
+      </div>
       <div className="container pb-5">
         <div className="row">
           <div className="col-md-6 col-lg-3">
@@ -15,20 +27,44 @@ function AboutPage() {
               />
               <div className="card-body">
                 <h5 className="card-title">Name</h5>
-                <h6 className="card-text"><i className="fa-sharp fa-solid fa-location-dot me-1"></i> Place</h6><hr/>
+                <h6 className="card-text">
+                  <i className="fa-sharp fa-solid fa-location-dot me-1"></i>{" "}
+                  Place
+                </h6>
+                <hr />
                 <h5 className="card-title">Backend Developer</h5>
-                <h6 className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia cumque minima modi voluptatibus, magni error culpa, voluptas rerum porro totam laborum neque incidunt dolorum?</h6>
-                <a href="https://www.whatsapp.com/" className="text-decoration-none text-reset social-links" id = "w">
-                    <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
+                <h6 className="card-text">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Mollitia cumque minima modi voluptatibus, magni error culpa,
+                  voluptas rerum porro totam laborum neque incidunt dolorum?
+                </h6>
+                <a
+                  href="https://www.whatsapp.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="w"
+                >
+                  <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
                 </a>
-                <a href="https://www.instagram.com/" className="text-decoration-none text-reset social-links" id = "i">
-                    <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
+                <a
+                  href="https://www.instagram.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="i"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
                 </a>
-                <a href="https://www.linkedin.com/" className="text-decoration-none text-reset social-links" id = "l">
-                    <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
+                <a
+                  href="https://www.linkedin.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="l"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
                 </a>
-                <a href="https://github.com/" className="text-decoration-none text-reset social-links" id = "g">
-                    <i className="fa-brands fa-2xl me-3 fa-github"></i>
+                <a
+                  href="https://github.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="g"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-github"></i>
                 </a>
               </div>
             </div>
@@ -41,21 +77,45 @@ function AboutPage() {
                 alt="img"
               />
               <div className="card-body">
-              <h5 className="card-title">Name</h5>
-                <h6 className="card-text"><i className="fa-sharp fa-solid fa-location-dot me-1"></i> Place</h6><hr/>
+                <h5 className="card-title">Name</h5>
+                <h6 className="card-text">
+                  <i className="fa-sharp fa-solid fa-location-dot me-1"></i>{" "}
+                  Place
+                </h6>
+                <hr />
                 <h5 className="card-title">Frontend Developer</h5>
-                <h6 className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia cumque minima modi voluptatibus, magni error culpa, voluptas rerum porro totam laborum neque incidunt dolorum?</h6>
-                <a href="https://www.whatsapp.com/" className="text-decoration-none text-reset social-links" id = "w">
-                    <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
+                <h6 className="card-text">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Mollitia cumque minima modi voluptatibus, magni error culpa,
+                  voluptas rerum porro totam laborum neque incidunt dolorum?
+                </h6>
+                <a
+                  href="https://www.whatsapp.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="w"
+                >
+                  <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
                 </a>
-                <a href="https://www.instagram.com/" className="text-decoration-none text-reset social-links" id = "i">
-                    <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
+                <a
+                  href="https://www.instagram.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="i"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
                 </a>
-                <a href="https://www.linkedin.com/" className="text-decoration-none text-reset social-links" id = "l">
-                    <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
+                <a
+                  href="https://www.linkedin.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="l"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
                 </a>
-                <a href="https://github.com/" className="text-decoration-none text-reset social-links" id = "g">
-                    <i className="fa-brands fa-2xl me-3 fa-github"></i>
+                <a
+                  href="https://github.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="g"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-github"></i>
                 </a>
               </div>
             </div>
@@ -68,21 +128,45 @@ function AboutPage() {
                 alt="img"
               />
               <div className="card-body">
-              <h5 className="card-title">Name</h5>
-                <h6 className="card-text"><i className="fa-sharp fa-solid fa-location-dot me-1"></i> Place</h6><hr/>
+                <h5 className="card-title">Name</h5>
+                <h6 className="card-text">
+                  <i className="fa-sharp fa-solid fa-location-dot me-1"></i>{" "}
+                  Place
+                </h6>
+                <hr />
                 <h5 className="card-title">Backend Developer</h5>
-                <h6 className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia cumque minima modi voluptatibus, magni error culpa, voluptas rerum porro totam laborum neque incidunt dolorum?</h6>
-                <a href="https://www.whatsapp.com/" className="text-decoration-none text-reset social-links" id = "w">
-                    <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
+                <h6 className="card-text">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Mollitia cumque minima modi voluptatibus, magni error culpa,
+                  voluptas rerum porro totam laborum neque incidunt dolorum?
+                </h6>
+                <a
+                  href="https://www.whatsapp.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="w"
+                >
+                  <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
                 </a>
-                <a href="https://www.instagram.com/" className="text-decoration-none text-reset social-links" id = "i">
-                    <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
+                <a
+                  href="https://www.instagram.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="i"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
                 </a>
-                <a href="https://www.linkedin.com/" className="text-decoration-none text-reset social-links" id = "l">
-                    <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
+                <a
+                  href="https://www.linkedin.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="l"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
                 </a>
-                <a href="https://github.com/" className="text-decoration-none text-reset social-links" id = "g">
-                    <i className="fa-brands fa-2xl me-3 fa-github"></i>
+                <a
+                  href="https://github.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="g"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-github"></i>
                 </a>
               </div>
             </div>
@@ -95,21 +179,45 @@ function AboutPage() {
                 alt="img"
               />
               <div className="card-body">
-              <h5 className="card-title">Name</h5>
-                <h6 className="card-text"><i className="fa-sharp fa-solid fa-location-dot me-1"></i> Place</h6><hr/>
+                <h5 className="card-title">Name</h5>
+                <h6 className="card-text">
+                  <i className="fa-sharp fa-solid fa-location-dot me-1"></i>{" "}
+                  Place
+                </h6>
+                <hr />
                 <h5 className="card-title">Frontend Developer</h5>
-                <h6 className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia cumque minima modi voluptatibus, magni error culpa, voluptas rerum porro totam laborum neque incidunt dolorum?</h6>
-                <a href="https://www.whatsapp.com/" className="text-decoration-none text-reset social-links" id = "w">
-                    <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
+                <h6 className="card-text">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Mollitia cumque minima modi voluptatibus, magni error culpa,
+                  voluptas rerum porro totam laborum neque incidunt dolorum?
+                </h6>
+                <a
+                  href="https://www.whatsapp.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="w"
+                >
+                  <i className="fa-brands mt-5 fa-2xl me-3 fa-whatsapp"></i>
                 </a>
-                <a href="https://www.instagram.com/" className="text-decoration-none text-reset social-links" id = "i">
-                    <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
+                <a
+                  href="https://www.instagram.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="i"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-instagram"></i>
                 </a>
-                <a href="https://www.linkedin.com/" className="text-decoration-none text-reset social-links" id = "l">
-                    <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
+                <a
+                  href="https://www.linkedin.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="l"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-linkedin"></i>
                 </a>
-                <a href="https://github.com/" className="text-decoration-none text-reset social-links" id = "g">
-                    <i className="fa-brands fa-2xl me-3 fa-github"></i>
+                <a
+                  href="https://github.com/"
+                  className="text-decoration-none text-reset social-links"
+                  id="g"
+                >
+                  <i className="fa-brands fa-2xl me-3 fa-github"></i>
                 </a>
               </div>
             </div>
